f568d1755ad1338c8edb3d5c0470f235b1941766
Bhuamik Gadani <bhuamikwork@gmail.com>
2016-03-09 15:20:02 +0530

    mailer config set for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,20 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'dispacher.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true 
+  ActionMailer::Base.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :domain => "Dispatch.com",
+    :authentication => :plain,
+    :user_name => "gujaratholidays",
+    :password => "gauravgarg",
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
