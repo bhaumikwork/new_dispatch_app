599f4d17c53629d0931fe22ff864c3923db6411e
Bhuamik Gadani <bhuamikwork@gmail.com>
2016-03-09 15:02:48 +0530

    home page and header added.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
 	def dashboard
 		
 	end
+	def home
+		
+	end
 end
--- app/models/dispatcher.rb
+++ app/models/dispatcher.rb
@@ -4,6 +4,11 @@ class Dispatcher < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+  def name
+    first_name.present? ? first_name : last_name.present? ? last_name : email
+  end
+
+  # on confirmation time ask for password for this below methods are override
   def password_required?
     super if confirmed?
   end
