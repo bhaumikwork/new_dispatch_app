3c7f1d5c7c63e9642e53f927042aa9028bb9c6c6
Bhuamik Gadani <bhuamikwork@gmail.com>
2016-03-22 14:57:43 +0530

    remove comments.

--- app/controllers/location_details_controller.rb
+++ app/controllers/location_details_controller.rb
@@ -1,66 +1,6 @@
 class LocationDetailsController < ApplicationController
-  before_action :set_location_detail, only: [:show, :edit, :update, :destroy]
   after_action :set_refresh_count,only: [:refresh_tracking_result,:tracking_result]
-
-  # GET /location_details
-  # GET /location_details.json
-  def index
-    @location_details = LocationDetail.all
-  end
-
-  # GET /location_details/1
-  # GET /location_details/1.json
-  def show
-  end
-
-  # GET /location_details/new
-  def new
-    @location_detail = LocationDetail.new
-  end
-
-  # GET /location_details/1/edit
-  def edit
-  end
-
-  # POST /location_details
-  # POST /location_details.json
-  def create
-    @location_detail = LocationDetail.new(location_detail_params)
-
-    respond_to do |format|
-      if @location_detail.save
-        format.html { redirect_to @location_detail, notice: 'Location detail was successfully created.' }
-        format.json { render :show, status: :created, location: @location_detail }
-      else
-        format.html { render :new }
-        format.json { render json: @location_detail.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /location_details/1
-  # PATCH/PUT /location_details/1.json
-  def update
-    respond_to do |format|
-      if @location_detail.update(location_detail_params)
-        format.html { redirect_to @location_detail, notice: 'Location detail was successfully updated.' }
-        format.json { render :show, status: :ok, location: @location_detail }
-      else
-        format.html { render :edit }
-        format.json { render json: @location_detail.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /location_details/1
-  # DELETE /location_details/1.json
-  def destroy
-    @location_detail.destroy
-    respond_to do |format|
-      format.html { redirect_to location_details_url, notice: 'Location detail was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  @@eta_time=1
 
   def location_detail_popup
     respond_to :js
@@ -68,9 +8,7 @@ class LocationDetailsController < ApplicationController
   def location_detail
     query = params[:address]
     Geocoder::Configuration.timeout = 10000
-    # @current_location = Geocoder.search(request.location.ip).first
     @dest_location = Geocoder.search(query).first
-    # set_source_and_dest_points(@current_location.latitude,@current_location.longitude,@dest_location.latitude,@dest_location.longitude)
     set_source_and_dest_points(params[:curr_lat],params[:curr_long],@dest_location.latitude,@dest_location.longitude)
     geteta
     unless @error
@@ -86,9 +24,6 @@ class LocationDetailsController < ApplicationController
         current_eta:@eta
       )
     end
-    # @location_detail = LocationDetail.first
-    # logger.info"<=sabbb====#{@current_location.data}======>"
-    # logger.info"<=sa====#{@current_location.latitude}====lll===#{@current_location.longitude}=>"
   end
 
   def geteta
@@ -123,19 +58,11 @@ class LocationDetailsController < ApplicationController
     else
       @is_api_limit_exceed = true if @location_detail.dispatcher_refresh_count > 4
     end
-    # if @location_detail.dispatcher == current_dispatcher
-      # @is_display = true if @location_detail.dispatcher_refresh_count < 3 && !@location_detail.is_terminate
-    # else
-    #   # @is_display = true if @location_detail.receiver_refresh_count < 3 && !@location_detail.is_terminate
-    #   @is_display = true if !@location_detail.is_terminate
-    # end
-    # if @is_display
-      set_source_and_dest_points(@location_detail.source_lat,@location_detail.source_long,@location_detail.dest_lat,@location_detail.dest_long)
-      @eta = @location_detail.eta
-      @eta_min = (@eta)%60
-      @eta_hr = (@eta)/60
-      set_timer_vars
-    # end
+    set_source_and_dest_points(@location_detail.source_lat,@location_detail.source_long,@location_detail.dest_lat,@location_detail.dest_long)
+    @eta = @location_detail.eta
+    @eta_min = (@eta)%60
+    @eta_hr = (@eta)/60
+    set_timer_vars
   end
 
   def refresh_tracking_result
@@ -145,12 +72,6 @@ class LocationDetailsController < ApplicationController
     else
       @is_api_limit_exceed = true if @location_detail.dispatcher_refresh_count > 4
     end
-    # if @location_detail.dispatcher == current_dispatcher
-      # @is_display = true if @location_detail.dispatcher_refresh_count < 3 && !@location_detail.is_terminate
-    # else
-    #   # @is_display = true if @location_detail.receiver_refresh_count < 3 && !@location_detail.is_terminate
-    #   @is_display = true if !@location_detail.is_terminate
-    # end
     if !@is_api_limit_exceed
       if @location_detail.dispatcher == current_dispatcher
         temp = @location_detail.update(curr_lat:params[:curr_lat],curr_long:params[:curr_long])
@@ -161,7 +82,7 @@ class LocationDetailsController < ApplicationController
       
       geteta
       @location_detail.update(current_eta: @eta,eta_calc_time:Time.zone.now) if @location_detail.dispatcher == current_dispatcher
-      if @eta <= 2
+      if @eta <= @@eta_time
         @location_detail.update(is_reached: true,current_eta: @eta) 
       end
       set_timer_vars
@@ -189,32 +110,9 @@ class LocationDetailsController < ApplicationController
       @dest_point= dest_lat.to_s+","+dest_long.to_s
     end
     def set_timer_vars
-      # logger.info"<=time====#{Time.zone}====#{Time.zone.now}=======#{@location_detail.eta_calc_time}==>"
-      # @timer_sec = ((Time.zone.now - @location_detail.eta_calc_time).round % 60)
-      # logger.info"<=time diff===#{@timer_sec}=======#{Time.zone.now - @location_detail.eta_calc_time}==>"
-      # diff = ((Time.zone.now - @location_detail.eta_calc_time)/60).round
-      # @tmp_eta = @location_detail.current_eta - (diff)
-      # @tmp_eta = 0 if @tmp_eta < 0
-      # logger.info"<=tmp eta===#{@tmp_eta}=======ll==>"
-      
-      # @timer_hr = @tmp_eta/60 
-      # @timer_min = @tmp_eta%60
-      # unless diff > 0 
-      #   @timer_sec = 55
-      #   @timer_min = @timer_min-1 unless @tmp_eta < @location_detail.current_eta
-      # end
-      # @timer_hr = @timer_hr < 0 ? 0 : @timer_hr
-      # @timer_min = @timer_min < 0 ? 1 : @timer_min
-
       @timer_secs = (@location_detail.current_eta * 60) - (Time.zone.now - @location_detail.eta_calc_time).round
       if @timer_secs <= 0
         @is_terminate = true
-        # @is_display = false
-        # @location_detail.update(is_terminate:true) if @timer_secs <= 0
-      # else
-      #   @timer_sec = @timer_secs % 60
-      #   @timer_min = ((@timer_secs-@timer_sec)%3600)/60
-      #   @timer_hr = @timer_secs/3600
       end
 
 
