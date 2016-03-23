9a38a269f7c5a5a1b40fd220d6605a64cc0c6064
Bhuamik Gadani <bhuamikwork@gmail.com>
2016-03-23 17:50:38 +0530

    add heroku run bash cmd.

--- screen_capture.js
+++ screen_capture.js
@@ -4,6 +4,8 @@ var page = require('webpage').create();
 console.log("screen_capture in js");
 console.log(args[1]);
 page.open('http://dispatcher-stagging.herokuapp.com/load_map/'+args[1], function() {
+// page.open('http://dispatcher-stagging.herokuapp.com/load_map/blfi7', function() {
+
   page.render('public/assets/'+args[1]+'.png');
   phantom.exit();
 });
