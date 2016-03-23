efacd24a5c54df3cd53ae04d745a5c00cd302c96
Bhuamik Gadani <bhuamikwork@gmail.com>
2016-03-23 17:30:20 +0530

    change image path

--- screen_capture.js
+++ screen_capture.js
@@ -4,7 +4,7 @@ var page = require('webpage').create();
 console.log("screen_capture in js");
 console.log(args[1]);
 page.open('http://dispatcher-stagging.herokuapp.com/load_map/'+args[1], function() {
-  page.render('public/location_detail/'+args[1]+'.png');
+  page.render('public/assets/'+args[1]+'.png');
   phantom.exit();
 });
 
