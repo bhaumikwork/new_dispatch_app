3325decc20d96ba963bc3d739b631edff273f34a
Bhuamik Gadani <bhuamikwork@gmail.com>
2016-03-23 18:54:08 +0530

    debug.

--- screen_capture.js
+++ screen_capture.js
@@ -3,11 +3,18 @@ var args = system.args;
 var page = require('webpage').create();
 console.log("screen_capture in js");
 console.log(args[1]);
-page.open('http://dispatcher-stagging.herokuapp.com/load_map/'+args[1], function() {
-// page.open('http://dispatcher-stagging.herokuapp.com/load_map/blfi7', function() {
-
-  page.render('./public/assets/'+args[1]+'.png');
-  console.log("image generated at ",'./public/assets/'+args[1]+'.png');
-  phantom.exit();
-});
+try {
+	page.open("https://google.co.in", function() {
+	// page.open('http://dispatcher-stagging.herokuapp.com/load_map/blfi7', function() {
+		try{
+			page.render('./public/assets/'+args[1]+'.png');
+		  console.log("image generated at ",'./public/assets/'+args[1]+'.png');
+		  phantom.exit();
+		} catch(x) {
+			console.log(x);
+		}
+	});
+} catch(e){
+	console.log(e);
+}
 
