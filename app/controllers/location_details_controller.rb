a7b9a65a7762d95d0665f44cea2cc1c4e4107836
Bhaumik Gadani <bhaumikwork@gmail.com>
2016-03-25 16:58:20 +0530

    remove condition for minus time

--- app/controllers/location_details_controller.rb
+++ app/controllers/location_details_controller.rb
@@ -85,7 +85,7 @@ class LocationDetailsController < ApplicationController
       if @eta <= $eta_time
         @location_detail.update(is_reached: true,current_eta: @eta) 
       end
-      set_terminate_var
+      # set_terminate_var
     else
       set_source_and_dest_points(@location_detail.curr_lat,@location_detail.curr_long,@location_detail.dest_lat,@location_detail.dest_long)
     end
