d52e73fbcbd01cc68c4d35e0d525a0338ed5e0ca
Bhuamik Gadani <bhuamikwork@gmail.com>
2016-03-25 16:31:51 +0530

    procfile added.

--- phantomjs_server/web.js
+++ phantomjs_server/web.js
@@ -90,7 +90,7 @@ app.post('/screenshot', function(request, response) {
 });
 
 
-var port = process.env.PORT || 5000;
+var port = 5000;//process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
